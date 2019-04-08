6d46ad1e3dfb0b158c9d8e329c5f91c04b2bcf1a
Iharidla <Ihar.Pikaliuk@itechart-group.com>
2019-04-08 15:22:13 +0300

    feat: add initial transaction

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,17 +7,28 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :rememberable, :validatable
 
+  private
+
   def create_banck_account
     currency_code = ISO3166::Country.new(self.country.iso).currency_code
     currency_id = Currency.find_by_abbreviation(currency_code).id
-    
+
     BankAccount.create(user_id: self.id, currency_id: currency_id)
     create_wallet(currency_code, currency_id)
   end
 
   def create_wallet(currency_code, currency_id)
+    wallet = Wallet.create(currency_id: currency_id, active: true, bank_account_id: self.bank_account.id)
+
+    create_initial_transaction(wallet, currency_code)
+  end
+
+  def create_initial_transaction(wallet, currency_code)
+    #add rate hardcoded for now
+    #need to add rates bettween all currencies
     Money.add_rate("USD", currency_code, 1.24515)
     balance = Money.us_dollar(1000).exchange_to(currency_code)
-    Wallet.create(balance: balance, currency_id: currency_id, active: true, bank_account_id: self.bank_account.id)
+
+    Transaction.create(to_wallet_id: wallet.id, balance: balance)
   end
 end
