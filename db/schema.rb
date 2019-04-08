99de748b0760bdb38747541bade0bda547b32c6d
dzmitry.kuliashou@itechart-group.com <itechart12345>
2019-04-08 12:59:39 +0300

    Add relations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_08_092352) do
+ActiveRecord::Schema.define(version: 2019_04_08_095833) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bank_accounts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.bigint "currency_id"
+    t.index ["currency_id"], name: "index_bank_accounts_on_currency_id"
+    t.index ["user_id"], name: "index_bank_accounts_on_user_id"
+  end
+
+  create_table "currencies", force: :cascade do |t|
+    t.string "name"
+    t.string "abbreviation"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,17 +41,15 @@ ActiveRecord::Schema.define(version: 2019_04_08_092352) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
-  create_table "currencies", force: :cascade do |t|
-    t.string "name"
-    t.string "abbreviation"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "wallets", force: :cascade do |t|
     t.float "balance"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "currency_id"
+    t.index ["currency_id"], name: "index_wallets_on_currency_id"
   end
 
+  add_foreign_key "bank_accounts", "currencies"
+  add_foreign_key "bank_accounts", "users"
+  add_foreign_key "wallets", "currencies"
 end
