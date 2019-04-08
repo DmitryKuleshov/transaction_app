d4dacfcb4c3aa47a44c2bceb2f6a838b01b7f0fd
Iharidla <Ihar.Pikaliuk@itechart-group.com>
2019-04-08 13:04:32 +0300

    feat: add users to country

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_08_092133) do
+ActiveRecord::Schema.define(version: 2019_04_08_094422) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +29,10 @@ ActiveRecord::Schema.define(version: 2019_04_08_092133) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "country_id"
+    t.index ["country_id"], name: "index_users_on_country_id"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "users", "countries"
 end
