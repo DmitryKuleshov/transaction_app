8969cf9d7fd968897d9689b441deff3aab34d748
Iharidla <Ihar.Pikaliuk@itechart-group.com>
2019-04-11 16:01:59 +0300

    feat: load all user transactions

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,7 +2,7 @@ class TransactionsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    to_wallet_ids = current_user.bank_account.wallets.pluck(:id)
-    @transactions = Transaction.where(to_wallet_id: to_wallet_ids)
+    wallet_ids = current_user.bank_account.wallets.pluck(:id)
+    @transactions = Transaction.where("to_wallet_id = ? OR from_wallet_id = ?", wallet_ids, wallet_ids)
   end
 end
